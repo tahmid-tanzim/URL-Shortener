333acb73a072f8b00a5dd17cf427ba933b92daa7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2022-02-28 11:19:09 +0600

    remove .env file from git tracking

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-44r%s9a3ba@0+krir_-@q!!1pfxkgh-j02n*i3q9)7a!1_p9v7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
